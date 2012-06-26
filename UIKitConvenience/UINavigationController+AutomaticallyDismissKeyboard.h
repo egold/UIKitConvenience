3f4536ff06cf8db4772e547fce2e827254207b18
Eric Goldberg <eric.goldberg@gmail.com>
2012-06-26 14:30:25 -0700

    Category to automatically dismiss keyboard (iPad) for UINavigationControllers presented modally

---
+++ UIKitConvenience/UINavigationController+AutomaticallyDismissKeyboard.h
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+AutomaticallyDismissKeyboard.h
+//  UIKitConvenience
+//
+//  Created by George Liang on 6/26/12.
+//  Copyright (c) 2012 Eric Goldberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationController (AutomaticallyDismissKeyboard)
+
+@end
---
+++ UIKitConvenience/UINavigationController+AutomaticallyDismissKeyboard.m
@@ -0,0 +1,18 @@
+//
+//  UINavigationController+AutomaticallyDismissKeyboard.m
+//  UIKitConvenience
+//
+//  Created by George Liang on 6/26/12.
+//  Copyright (c) 2012 Eric Goldberg. All rights reserved.
+//
+
+#import "UINavigationController+AutomaticallyDismissKeyboard.h"
+
+@implementation UINavigationController (AutomaticallyDismissKeyboard)
+
+-(BOOL)disablesAutomaticKeyboardDismissal
+{
+    return NO;
+}
+
+@end
